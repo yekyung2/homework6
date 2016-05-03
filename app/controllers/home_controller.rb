99067a59c591abd9948e89700cdf8e8b38ea2d73
yekyung2 <lamp_lame@naver.com>
2016-05-03 04:56:59 +0000

    new

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,19 @@
+class HomeController < ApplicationController
+  def index
+    @posts=Post.all
+  end
+  def reply_write
+    reply=Reply.new
+    reply.content=params[:content]
+    reply.post_id=params[:id_of_post]
+    reply.save
+    redirect_to "/home/index"
+  end
+  def write
+    post=Post.new
+    post.title=params[:title]
+    post.content=params[:content]
+    post.save
+    redirect_to "/home/index"
+  end
+end
